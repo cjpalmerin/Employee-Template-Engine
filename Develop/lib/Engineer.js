d909c2e65eefb01df42db2d1d9b9371e9b22f513
CJPALME <cjpalmerin@gmail.com>
2020-04-21 20:28:55 -0500

    Created classes and subclasses for Employees, Managers, Engineers, and Interns.

---
+++ Develop/lib/Engineer.js
@@ -0,0 +1,15 @@
+// TODO: Write code to define and export the Engineer class.  HINT: This class should inherit from Employee.
+class Engineer extends Employee{
+    constructor (name, id, email, githubUsername) {
+    super(name, id, email);
+    this.githubUsername = githubUsername
+    }
+    getGithub() {
+        return this.githubUsername
+    }
+    getRole() {
+        return 'Engineer'
+    }
+}
+
+module.exports = Engineer;
