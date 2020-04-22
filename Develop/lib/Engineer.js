99172d8e4e33940c588615fa896d32736fce584a
CJPALME <cjpalmerin@gmail.com>
2020-04-21 20:49:27 -0500

    Completed tests on classes and fixed errors with importing Employee class.

--- Develop/lib/Engineer.js
+++ Develop/lib/Engineer.js
@@ -1,11 +1,12 @@
-// TODO: Write code to define and export the Engineer class.  HINT: This class should inherit from Employee.
+const Employee = require('./Employee')
+
 class Engineer extends Employee{
-    constructor (name, id, email, githubUsername) {
+    constructor (name, id, email, github) {
     super(name, id, email);
-    this.githubUsername = githubUsername
+    this.github = github
     }
     getGithub() {
-        return this.githubUsername
+        return this.github
     }
     getRole() {
         return 'Engineer'
